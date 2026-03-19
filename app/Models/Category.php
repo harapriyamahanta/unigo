22051f064163db0cac00f9a043c829a128da1596
Your Name <you@example.com>
2026-03-19 20:52:05 +0530

    subcategorylist

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     public function subcategory(){
-        return $this->belongsToMany('App\Models\SubCategory', 'category_id')->select('name','image');
+        return $this->belongsToMany('App\Models\SubCategory', 'category_id','id')->select('name','image');
     }
 }
