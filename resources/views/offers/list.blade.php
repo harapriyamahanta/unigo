fcb9fbef021aa11691610990e57754276de74a44
Your Name <you@example.com>
2026-03-22 10:14:37 +0530

    add editor

--- resources/views/offers/list.blade.php
+++ resources/views/offers/list.blade.php
@@ -9,7 +9,7 @@
                 </i>
             </a>
         </div>
-        <div class="sliding custom-title">Offers</div>
+        <div class="sliding custom-title">Blogs</div>
         <div class="right d-flex">
             <a href="javascript:void(0)" onClick="openAdd()" class="link icon-only" title="Add Category"><i class="material-icons">add</i></a>
             <a href="#" data-bs-toggle="dropdown" aria-expanded="true" class="link"><i class="material-icons">more_vert</i></a>
@@ -28,7 +28,7 @@
                             </i> Back
                         </div> -->
                            
-                        <div class="user">{{count($offers)}} Offers</div>
+                        <div class="user">{{count($offers)}} Blogs</div>
                     </div>
                    
                     <!-- Searchbar with auto Search -->
@@ -49,7 +49,7 @@
                                 </div>
                                 <div class="item-cat">
                                     <div class="item-title">
-                                        {{$cat->title}}
+                                        <a href="{{url('sub-categories/'.$cat->id)}}">{{$cat->title}}</a>
                                     </div>
                                 </div>
                                 <div class="right d-flex">                    
@@ -90,9 +90,8 @@
                                                     <span class="cam-icon"><img src="assets/img/placeholder-small.svg" alt=""></span>
                                                 </a>
                                             </div> -->
-                                           <form action="{{url('/store-offer')}}" id="myForm" method="post"  enctype="multipart/form-data">
+                                           <form action="{{url('/store-blogs')}}" id="myForm" method="post"  enctype="multipart/form-data">
                                                 @csrf
-                                                
                                                 <ul><li>
                                                     <div class="file-upload">
                                                         <img id="output" src="assets/img/doctors/doctor-thumb-02.jpg" class="img-fluid img-circle file-upload-img" width="300" height="300" alt="User Image">
@@ -115,49 +114,20 @@
                                                     </li>
                                                     <li class="item-content item-input">
                                                         <div class="item-col">
-                                                            <div class="item-title item-label">Type <span>*</span></div>
+                                                            <div class="item-title item-label">Tag <span>*</span></div>
                                                             <div class="item-input-wrap">
-                                                                <select id="type" name="type" required onchange="hideShowCompany()">
-                                                                    <option>app</option>
-                                                                    <option>external</option>
+                                                                <select id="tag" name="tag" required>
+                                                                    <option>Travel</option>
                                                                 </select>
                                                                 <span class="input-clear-button"></span>
                                                             </div>
                                                         </div>
                                                     </li> 
-                                                    <li class="item-content item-input">
+                                                     <li class="item-content item-input">
                                                         <div class="item-col">
-                                                            <div class="item-title item-label">Start Date <span>*</span></div>
+                                                            <div class="item-title item-label">Date <span>*</span></div>
                                                             <div class="item-input-wrap">
-                                                                <input type="date"  name="startDate" id="startDate" required> 
-                                                                <span class="input-clear-button"></span>
-                                                            </div>
-                                                        </div>
-                                                    </li>
-                                                    <li class="item-content item-input">
-                                                        <div class="item-col">
-                                                            <div class="item-title item-label">End Date <span>*</span></div>
-                                                            <div class="item-input-wrap">
-                                                                <input type="date"  name="endDate" id="endDate" required> 
-                                                                <span class="input-clear-button"></span>
-                                                            </div>
-                                                        </div>
-                                                    </li>
-                                                    
-                                                    <li class="item-content item-input">
-                                                        <div class="item-col">
-                                                            <div class="item-title item-label">Code <span>*</span></div>
-                                                            <div class="item-input-wrap">
-                                                                <input type="text"  name="code" id="code" required > 
-                                                                <span class="input-clear-button"></span>
-                                                            </div>
-                                                        </div>
-                                                    </li>
-                                                    <li class="item-content item-input">
-                                                        <div class="item-col">
-                                                            <div class="item-title item-label">Terms <span>*</span></div>
-                                                            <div class="item-input-wrap">
-                                                                <textarea   name="terms" id="terms" required> </textarea>
+                                                                <input type="date"  name="date" id="date" required> 
                                                                 <span class="input-clear-button"></span>
                                                             </div>
                                                         </div>
@@ -171,62 +141,11 @@
                                                                 required
                                                                 name="description"
                                                              placeholder="">
-                                                            </textarea>
+</textarea>
                                                         </div>
                                                     </div>
-                                                </li>   
-                                                </ul>
-                                                                                          
-                                                <ul class="companyfield">
-                                                    <li class="item-content item-input">
-                                                        <div class="item-col">
-                                                            <div class="item-title item-label">Company Name </div>
-                                                            <div class="item-input-wrap">
-                                                                <input type="text"  name="companyName" id="companyName"  > 
-                                                                <span class="input-clear-button"></span>
-                                                            </div>
-                                                        </div>
-                                                    </li>
-                                                    <li class="item-content item-input">
-                                                        <div class="item-col">
-                                                            <div class="item-title item-label">Phone </div>
-                                                            <div class="item-input-wrap">
-                                                                <input type="text"  name="phone" id="phone"  > 
-                                                                <span class="input-clear-button"></span>
-                                                            </div>
-                                                        </div>
-                                                    </li>
-                                                    <li class="item-content item-input">
-                                                        <div class="item-col">
-                                                            <div class="item-title item-label">Website </div>
-                                                            <div class="item-input-wrap">
-                                                                <input type="text"  name="website" id="website"  > 
-                                                                <span class="input-clear-button"></span>
-                                                            </div>
-                                                        </div>
-                                                    </li>
-                                                    <li class="item-content item-input">
-                                                        <div class="item-col">
-                                                            <div class="item-title item-label">Address </div>
-                                                            <div class="item-input-wrap">
-                                                                <input type="text"  name="address" id="address"  > 
-                                                                <span class="input-clear-button"></span>
-                                                            </div>
-                                                        </div>
-                                                    </li>
-                                                </ul> 
-                                                <!-- <ul class="companyfield">
-                                                    <li>
-                                                     <h5>Upload Company Logo</h5>
-                                                    <div class="file-upload">
-                                                        <img id="outputcompanyLogo" src="assets/img/doctors/doctor-thumb-02.jpg" class="img-fluid img-circle file-upload-img" width="300" height="300" alt="User Image">
+                                                </li>                                            
                                                     
-                                                    <input type="file"  name="companyLogo" accept="image/*" onchange="loadFilecompanyLogo(event)"  />
-                                                    <span class="input-clear-button"></span>
-                                                    </div>
-                                                    </li>
-                                                </ul>                                           -->
-                                                   <ul> 
                                                     <li class="bottom-button">
                                                         <button  class="btn addZoneBtn">Add Blog</button>
                                                     </li>
@@ -278,47 +197,26 @@ function openEdit (item){
     $('.title').html('Edit Blog');
     $('.addZoneBtn').html('Update Blog');
     $('#title').val(item.title);
-    $('#type').val(item.type);
-    $('#code').val(item.code);
-    $('#startDate').val(item.startDate);
-    $('#endDate').val(item.endDate);
-    $('#terms').val(item.terms);
-    $('#companyName').val(item.companyName);
-    $('#phone').val(item.phone);
-    $('#address').val(item.address);
-    $('#website').val(item.website);
+    $('#date').val(item.date);
+    $('#tag').val(item.tag);
+    $('#desc').val(item.description);
     editor1.setHTMLCode(item.description);
     var output = document.getElementById('output');
-    output.src = item.image;
-    var outputlogo = document.getElementById('outputcompanyLogo');
-    if(outputlogo){
-    outputlogo.src = item.companyLogo;
-    }
+    output.src = item.imageUrl;
 }
 function openAdd (){
    // console.log(item,'item');
     $('#zone-modal').modal('show');
     $('#zone_id').val('');
-    $('.title').html('Add Offer');
-    $('.addZoneBtn').html('Add Offer');
-    $('#title').val('');
-    $('#type').val('');
-    $('#code').val('');
-    $('#startDate').val('');
-    $('#endDate').val('');
-    $('#terms').val('');
-    $('#companyName').val('');
-    $('#phone').val('');
-    $('#address').val('');
-    $('#website').val('');
+    $('.title').html('Add Blog');
+    $('.addZoneBtn').html('Add Blog');
+     $('#title').val('');
+    $('#date').val('');
+    $('#tag').val('');
+    $('#desc').val('');
     editor1.setHTMLCode("");
     var output = document.getElementById('output');
     output.src = 'assets/img/doctors/doctor-thumb-02.jpg';
-
-    var outputcompanyLogo = document.getElementById('outputcompanyLogo');
-    if(outputcompanyLogo){
-    outputcompanyLogo.src = 'assets/img/doctors/doctor-thumb-02.jpg';
-    }
 }
 var loadFile = function(event) {
     var output = document.getElementById('output');
@@ -327,23 +225,17 @@ var loadFile = function(event) {
       URL.revokeObjectURL(output.src) // free memory
     }
   };
-  var loadFilecompanyLogo = function(event) {
-    var output = document.getElementById('outputcompanyLogo');
-    output.src = URL.createObjectURL(event.target.files[0]);
-    output.onload = function() {
-      URL.revokeObjectURL(output.src) // free memory
-    }
-  };
-
-  function hideShowCompany(){
-    var type = $('#type').val();
-    if(type=='external'){
-        $('.companyfield').show();
-    }else{
-        $('.companyfield').hide();
-    }
+  function beforeSubmit(){
+    const form = document.querySelector('form');
+form.addEventListener('formdata', (event) => {
+  // Append Quill content before submitting
+  event.formData.append('about', JSON.stringify(quill.getContents().ops));
+  $('#descTextarea').val(JSON.stringify(quill.getContents().ops));
+  console.log(JSON.stringify(quill.getContents().ops));
+});
+form.submit();
+document.getElementById('myForm').submit();
   }
-  
 </script>
 
 @endsection
