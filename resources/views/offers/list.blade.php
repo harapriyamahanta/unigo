579e6c65fc080b90cfc789297ce7d1a9fbd6aa94
Your Name <you@example.com>
2026-03-22 09:59:58 +0530

    add editor

--- resources/views/offers/list.blade.php
+++ resources/views/offers/list.blade.php
@@ -177,17 +177,7 @@
                                                     </div>
                                                 </li>   
                                                 </ul>
-                                                <ul class="companyfield">
-                                                    <li>
-                                                     <h5>Upload Company Logo</h5>
-                                                    <div class="file-upload">
-                                                        <img id="outputcompanyLogo" src="assets/img/doctors/doctor-thumb-02.jpg" class="img-fluid img-circle file-upload-img" width="300" height="300" alt="User Image">
-                                                    
-                                                    <input type="file"  name="companyLogo" accept="image/*" onchange="loadFilecompanyLogo(event)"  />
-                                                    <span class="input-clear-button"></span>
-                                                    </div>
-                                                    </li>
-                                                </ul>                                           
+                                                                                          
                                                 <ul class="companyfield">
                                                     <li class="item-content item-input">
                                                         <div class="item-col">
@@ -224,6 +214,17 @@
                                                                 <span class="input-clear-button"></span>
                                                             </div>
                                                         </div>
+                                                    </li>
+                                                </ul> 
+                                                <ul class="companyfield">
+                                                    <li>
+                                                     <h5>Upload Company Logo</h5>
+                                                    <div class="file-upload">
+                                                        <img id="outputcompanyLogo" src="assets/img/doctors/doctor-thumb-02.jpg" class="img-fluid img-circle file-upload-img" width="300" height="300" alt="User Image">
+                                                    
+                                                    <input type="file"  name="companyLogo" accept="image/*" onchange="loadFilecompanyLogo(event)"  />
+                                                    <span class="input-clear-button"></span>
+                                                    </div>
                                                     </li>
                                                 </ul>                                          
                                                    <ul> 
